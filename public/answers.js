49666832761a770eb1fb3672660ef610088fe06b
Thomas <thomas.gauthier.c@gmail.com>
2022-11-16 11:10:00 -0500

    gems integration

---
+++ public/answers.js
@@ -0,0 +1,154 @@
+
+const answers = {}
+
+let answerer = "";
+
+window.answers = answers;
+
+
+const questions = {
+    upOrDown: 'Art is the closest I’ve come to feeling that we might understandeach other. Like we might actually touch. Do you feel connected through art?',
+    qSearchForPeople: 'How do you search for your audience? How do you reach for them?',
+    qMoreFans: 'Do you want a bigger audience? Or stronger ties with a smaller audience? Or is what you have enough? Why?',
+    qMaterialChange: 'Could you invite people more deeply into your work? How could you make space for them to materially change your art?',
+    qDeepListen: 'In what ways do you listen to your audience? How do you know when they feel moved? When they feel left behind?',
+    qTransformWork: 'How far do you go in transforming your work to be understood? How far should you go? ',
+    qLoseEnergy: 'Is it worth it to reach people if you lose your own artistic vigor on the way?',
+    darkOrJoy: 'Does your audience cocreate art with you?',
+    outOrIn: 'Are you searching for connection?',
+    chaosOrCalm: 'Is art about the artist?',
+    forestOrMeadow: 'Do you take your audience by the hand and pull them in?',
+    morningOrNight: 'Will you delightedly translate your work to be understood?',
+    unfoldOrCycle: 'Is public art a dialogue? Are you in conversation with other people?'
+}
+
+
+let myAnswers = []
+
+document.getElementById('seekText').addEventListener('input', (e) => {
+    answerer = e.target.value;
+})
+
+
+const textAreas = ['qSearchForPeople', 'qMoreFans', 'qMaterialChange', 'qDeepListen', 'qTransformWork', 'qLoseEnergy']
+
+textAreas.forEach(id => {
+    document.getElementById(id).addEventListener('input', (e) => {
+        answers[id] = e.target.value
+    })
+})
+
+document.addEventListener('answer', (e) => {
+    const answerMap = {
+        'up' : 'yes',
+        'down' : 'no',
+        'dark' : 'yes',
+        'joy' : 'no',
+        'chaos' : 'yes',
+        'calm' : 'no',
+        'forest' : 'yes',
+        'meadow' : 'no',
+        'out' : 'yes',
+        'in' : 'no',
+        'morning' : 'yes',
+        'night' : 'no',
+        'unfold' : 'yes',
+        'cycle' : 'no',
+    }
+
+    answers[e.detail.question] = answerMap[e.detail.value];
+}, false);
+
+document.getElementById('findSongBtn').addEventListener('click', async () => {
+
+    const res = await fetch('/answers', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            answerer,
+            answers
+        })
+    });
+
+
+    myAnswers = await res.json();
+    const answersRes = await fetch('/answers');
+    renderGems(await answersRes.json())
+
+}, { once: true })
+
+
+function renderGems(answers) {
+    const questionss = {}
+
+    answers.forEach(({ id, answerer, questionID, answer, targetX, targetY }, idx) => {
+
+        if (!(questionID in questionss)) {
+            questionss[questionID] = Math.random() * 360;
+        }
+
+
+        const gem = document.createElement('img');
+        gem.classList.add('gem')
+
+        gem.setAttribute('style', `--animationTime : ${Math.random() * 10}s; --baseHue: ${questionss[questionID]}deg;`)
+
+        // gem.src = `images/gems/${questionID}.png`
+        gem.src = `images/gems/upOrDown.png`
+
+
+        const container = document.createElement('div')
+        container.classList.add('answer-gem_container')
+
+        const div = document.createElement('div');
+        container.appendChild(div)
+        div.classList.add('answer-gem')
+        div.id = `answer-${id}`;
+
+
+        if (myAnswers.find(a => a.id == id)) {
+            gem.classList.add('own')
+        }
+
+        div.style.position = 'relative';
+        container.style.width = 'fit-content';
+
+        const x = Math.random() * (window.innerWidth - 100);
+        const y = Math.random() * (500);
+
+
+        container.style.position = 'absolute';
+        container.style.left = `${(targetX + (Math.random() - 0.5) / 5) * 100}%`
+        container.style.top = `${(targetY + (Math.random() - 0.5) / 5) * 100}%`
+
+        container.style.setProperty('--slowMoveOffset', `${Math.random()}s`)
+        container.style.setProperty('--rotateShift',0)// `${Math.random() * 360}deg`)
+        container.style.setProperty('--slowMoveTime', `${Math.random()}s`)
+
+        div.appendChild(gem);
+
+        const tooltip = document.createElement('div');
+        tooltip.classList.add('answer-tooltip');
+
+        const question = questions[questionID] || ''
+        tooltip.innerHTML = `<span class="answer-tooltip__answerer">${answerer}</span><br><div class="answer-tooltip__question">${question}</div><br>${answer}`;
+        div.appendChild(tooltip);
+
+        div.addEventListener('pointerenter', () => {
+            tooltip.style.display = 'block';
+        })
+
+        div.addEventListener('pointerleave', () => {
+            tooltip.style.display = 'none';
+        })
+
+        document.getElementById('lastLvl').appendChild(container);
+    })
+
+    if (!document.querySelector('[src="dragGems.js"]')) {
+        var script = document.createElement("script");
+        script.setAttribute("type", "text/javascript");
+        script.setAttribute("src", "dragGems.js");
+        document.getElementsByTagName("head")[0].appendChild(script);
+    }
+}
---
+++ public/dragGems.js
@@ -0,0 +1,104 @@
+var gems = [...document.querySelectorAll(".answer-gem")];
+
+const handlers = {}
+
+document.addEventListener("touchstart", (e) => {
+    handlers[e.target?.id] && handlers[e.target?.id].dragStart(e);
+    handlers[e.target.parentElement?.id] && handlers[e.target.parentElement?.id].dragStart(e);
+}, { passive: false });
+document.addEventListener("touchend", (e) => {
+    handlers[e.target?.id] && handlers[e.target?.id].dragEnd(e);
+    handlers[e.target.parentElement?.id] && handlers[e.target.parentElement?.id].dragEnd(e);
+}, { passive: false });
+document.addEventListener("touchmove", (e) => {
+    handlers[e.target?.id] && handlers[e.target?.id].drag(e);
+    handlers[e.target.parentElement?.id] && handlers[e.target.parentElement?.id].drag(e);
+}, { passive: false });
+
+document.addEventListener("mousedown", (e) => {
+    console.log(e.target, e.target.parentElement?.id, handlers[e.target.parentElement?.id]);
+    handlers[e.target?.id] && handlers[e.target?.id].dragStart(e);
+    handlers[e.target.parentElement?.id] && handlers[e.target.parentElement?.id].dragStart(e);
+}, { passive: false });
+document.addEventListener("mouseup", (e) => {
+    activeHandler?.dragEnd(e);
+}, { passive: false });
+document.addEventListener("mousemove", (e) => {
+    activeHandler?.drag(e);
+}, { passive: false });
+
+
+let activeHandler;
+
+function setTranslate(xPos, yPos, el) {
+    if (el.style.transform && !el.theTransform) {
+        el.theTransform = el.style.transform;
+    }
+    el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+}
+
+gems.forEach((gem) => {
+    var active = false;
+    var currentX;
+    var currentY;
+    var initialX;
+    var initialY;
+    var xOffset = 0;
+    var yOffset = 0;
+
+    const img = gem.querySelector('img')
+    const id = gem?.id;
+
+    const handler = {
+        dragStart,
+        drag,
+        dragEnd
+    };
+
+    function dragStart(e) {
+        if (e.target == gem || e.target == img) {
+            e.preventDefault();
+            active = true;
+            activeHandler = handler;
+        }
+
+        if (e.type === "touchstart") {
+            initialX = e.touches[0].clientX - xOffset;
+            initialY = e.touches[0].clientY - yOffset;
+        } else {
+            initialX = e.clientX - xOffset;
+            initialY = e.clientY - yOffset;
+        }
+    }
+
+    function dragEnd(e) {
+        initialX = currentX;
+        initialY = currentY;
+
+        active = false;
+        activeHandler = null;
+    }
+
+    function drag(e) {
+        if (active) {
+
+            e.preventDefault();
+
+            if (e.type === "touchmove") {
+                currentX = e.touches[0].clientX - initialX;
+                currentY = e.touches[0].clientY - initialY;
+            } else {
+                currentX = e.clientX - initialX;
+                currentY = e.clientY - initialY;
+            }
+
+            xOffset = currentX;
+            yOffset = currentY;
+
+            setTranslate(currentX, currentY, gem);
+        }
+    }
+
+
+    handlers[id] = handler
+})
