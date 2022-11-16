b5c204dac409b94db92edb4862eb53c7c9e81375
Thomas <thomas.gauthier.c@gmail.com>
2022-11-16 12:39:49 -0500

    added content moderation page

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
-import { Prisma, PrismaClient } from '@prisma/client'
-import express from 'express'
+import { Prisma, PrismaClient } from "@prisma/client";
+import "dotenv/config"
+import express from "express";
+import basicAuth from 'express-basic-auth'
 
-const prisma = new PrismaClient()
-const app = express()
+const prisma = new PrismaClient();
+const app = express();
 
-app.use(express.json())
-app.use(express.static('public'))
+let auth = basicAuth({
+  users: { 'admin': process.env.PASSWORD || 'password' },
+  challenge: true,
+});
+
+app.use('/database', auth)
+app.use('/delete/', auth)
+
+
+app.use(express.json());
+app.use(express.static("public"));
 
 app.put(`/answers`, async (req, res) => {
-
-
-  const answers = []
+  const answers = [];
 
   const questions = Object.keys(req.body.answers);
 
@@ -22,47 +31,108 @@ app.put(`/answers`, async (req, res) => {
         data: {
           questionID: question,
           answer,
-          answerer: req.body.answerer
-        }
-      })
+          answerer: req.body.answerer,
+        },
+      });
 
-      answers.push(answered)
+      answers.push(answered);
     } catch (e: any) {
-      res.status(500).send(e.toString && e.toString())
+      res.status(500).send(e.toString && e.toString());
       return;
     }
   }
 
   res.json(answers);
-})
-
+});
 
 const targets = new Map();
 
-app.get('/answers', async (req, res) => {
+app.get("/answers", async (req, res) => {
   const answers = await prisma.answer.findMany({});
 
-
-  answers.forEach(answer => {
-    const id = `${answer.questionID}${answer.answer}`
+  answers.forEach((answer) => {
+    const id = `${answer.questionID}${answer.answer}`;
     if (!targets.has(id)) {
-      targets.set(id, { x: Math.random(), y: Math.random() })
+      targets.set(id, { x: Math.random(), y: Math.random() });
     }
-  })
+  });
 
+  res.json(
+    answers.map((a) => {
+      const id = `${a.questionID}${a.answer}`;
 
+      const target = targets.get(id);
+      return { ...a, targetX: target.x, targetY: target.y };
+    })
+  );
+});
 
-  res.json(answers.map(a => {
-    const id = `${a.questionID}${a.answer}`
+app.post("/delete/:id", async (req, res) => {
+  await prisma.answer.delete({
+    where: {
+      id: parseInt(req.params.id),
+    },
+  });
 
-    const target = targets.get(id)
-    return { ...a, targetX: target.x, targetY: target.y }
-  }))
-})
+  res.redirect("/database");
+});
 
+app.get("/database", async (req, res) => {
+  // { id, answerer, questionID, answer}
 
+  const answers = await prisma.answer.findMany({});
+  res.setHeader("Content-Type", "text/html");
+  res.send(`
+  <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>title</title>
+    <style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+  </head>
+  <body>
+  <table>
+  <tr>
+    <th>id</th>
+    <th>answerer</th>
+    <th>questionID</th>
+    <th>answer</th>
+  </tr>
+  ${answers
+      .map(
+        ({ id, answerer, questionID, answer }) => `<tr>
+  <td>${id}</td>
+  <td>${answerer}</td>
+  <td>${questionID}</td>
+  <td>${answer}</td>
+  <td>
+  <form action="/delete/${id}" method="POST">
+  <input type="submit" value="Delete">
+</form> </td>
+</tr>`
+      )
+      .join("")}
+  </table>
+  </body>
+</html>`);
+});
 
 const server = app.listen(process.env.PORT || 3000, async () => {
-  console.log(`🚀 Server ready at: http://localhost:3000`)
-},
-)
+  console.log(`🚀 Server ready at: http://localhost:3000`);
+});
